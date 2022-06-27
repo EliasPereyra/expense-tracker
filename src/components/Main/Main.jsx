cb2924f584cbf14a8cc9142c6d7c3e0b25a8422b
Elías Ezequiel Pereyra Gómez <EliasPereyra_Gomez@hotmail.com>
2022-06-27 12:14:35 +0000

    Better formatting for the Main component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,40 +1,55 @@
-import React, { useContext } from 'react'
+import React, { useContext } from "react";
+import { useSpeechContext } from "@speechly/react-client";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  Grid,
+  Divider,
+} from "@material-ui/core";
 
-import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core'
+import { ExpenseTrackerContext } from "../../context/context";
 
-import {ExpenseTrackerContext} from '../../context/context';
-import {useSpeechContext} from '@speechly/react-client';
+import Form from "./Form/Form";
+import List from "./List/List";
+import InfoCard from "../InfoCard";
 
-import Form from './Form/Form'
-import List from './List/List'
-import InfoCard from '../InfoCard'
-
-import useStyles from './styles';
+import useStyles from "./styles";
 
 const ExpenseTracker = () => {
-    const classes = useStyles();
-    const {balance} = useContext(ExpenseTrackerContext);
+  const classes = useStyles();
+  const { balance } = useContext(ExpenseTrackerContext);
 
-    return (
-        <Card className={classes.root}>
-            <CardHeader className={classes.header} title="Expense Tracker" subheader="Powered by Speechly" />
-            <CardContent>
-                <Typography align="center" variant="h5">Total Balance: <span style={{fontWeight: 'bold'}}>${balance}</span></Typography>
-                <Typography variant="subtitle1" style={{lineHeight: '1.5em', marginTop: '20px'}}>
-                    <InfoCard />
-                </Typography>
-                <Divider className={classes.divider} />
-                <Form />
-            </CardContent>
-            <CardContent className={classes.cartContent}>
-                <Grid container spacing={2}> 
-                    <Grid item xs={12}>
-                        <List />
-                    </Grid>
-                </Grid>
-            </CardContent>
-        </Card>
-    );
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        className={classes.header}
+        title="Expense Tracker"
+        subheader="Powered by Speechly"
+      />
+      <CardContent>
+        <Typography align="center" variant="h5">
+          Total Balance: <span style={{ fontWeight: "bold" }}>${balance}</span>
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          style={{ lineHeight: "1.5em", marginTop: "20px" }}
+        >
+          <InfoCard />
+        </Typography>
+        <Divider className={classes.divider} />
+        <Form />
+      </CardContent>
+      <CardContent className={classes.cartContent}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <List />
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
 };
 
-export default ExpenseTracker
+export default ExpenseTracker;
